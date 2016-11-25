3833cec855b3443134c767673d2c887c13a84ebe
eugene <kashirin.e@list.ru>
2016-11-25 14:40:16 +0300

    Add test file and resources

---
+++ src/test/java/replication/FakeReplicaTest.java
@@ -0,0 +1,12 @@
+package replication;
+
+import org.junit.Test;
+
+/**
+ * Created by eugene on 11/25/16.
+ */
+public class FakeReplicaTest {
+  @Test
+  public void fullState() throws Exception{
+  }
+}
