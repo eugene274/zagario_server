d7afad5e1f85e810fcd6949e0d057730f93be6d5
Kashirin Eugene <kashirin.e@list.ru>
2016-12-18 13:31:59 +0300

    Add MathVector class

---
+++ src/main/java/utils/MathVector.java
@@ -0,0 +1,152 @@
+package utils;
+
+/******************************************************************************
+ *  Compilation:  javac Vector.java
+ *  Execution:    java Vector
+ *
+ *  Implementation of a vector of real numbers.
+ *
+ *  This class is implemented to be immutable: once the client program
+ *  initialize a Vector, it cannot change any of its fields
+ *  (N or data[i]) either directly or indirectly. Immutability is a
+ *  very desirable feature of a data type.
+ *
+ *
+ *  % java Vector
+ *  x        =  (1.0, 2.0, 3.0, 4.0)
+ *  y        =  (5.0, 2.0, 4.0, 1.0)
+ *  x + y    =  (6.0, 4.0, 7.0, 5.0)
+ *  10x      =  (10.0, 20.0, 30.0, 40.0)
+ *  |x|      =  5.477225575051661
+ *  <x, y>   =  25.0
+ *  |x - y|  =  5.0990195135927845
+ *
+ *  Note that java.util.Vector is an unrelated Java library class.
+ *
+ ******************************************************************************/
+
+public class MathVector {
+
+    private final int n;         // length of the vector
+    private double[] data;       // array of vector's components
+
+    // create the zero vector of length n
+    public MathVector(int n) {
+        this.n = n;
+        this.data = new double[n];
+    }
+
+    // create a vector from an array
+    public MathVector(double[] data) {
+        n = data.length;
+
+        // defensive copy so that client can't alter our copy of data[]
+        this.data = new double[n];
+        for (int i = 0; i < n; i++)
+            this.data[i] = data[i];
+    }
+
+    // create a vector from either an array or a vararg list
+    // this constructor uses Java's vararg syntax to support
+    // a constructor that takes a variable number of arguments, such as
+    // Vector x = new Vector(1.0, 2.0, 3.0, 4.0);
+    // Vector y = new Vector(5.0, 2.0, 4.0, 1.0);
+/*
+    public Vector(double... data) {
+        n = data.length;
+
+        // defensive copy so that client can't alter our copy of data[]
+        this.data = new double[n];
+        for (int i = 0; i < n; i++)
+            this.data[i] = data[i];
+    }
+*/
+    // return the length of the vector
+    public int length() {
+        return n;
+    }
+
+    // return the inner product of this Vector a and b
+    public double dot(MathVector that) {
+        if (this.length() != that.length())
+            throw new IllegalArgumentException("dimensions disagree");
+        double sum = 0.0;
+        for (int i = 0; i < n; i++)
+            sum = sum + (this.data[i] * that.data[i]);
+        return sum;
+    }
+
+    // return the Euclidean norm of this Vector
+    public double magnitude() {
+        return Math.sqrt(this.dot(this));
+    }
+
+    // return the Euclidean distance between this and that
+    public double distanceTo(MathVector that) {
+        if (this.length() != that.length())
+            throw new IllegalArgumentException("dimensions disagree");
+        return this.minus(that).magnitude();
+    }
+
+    // return this + that
+    public MathVector plus(MathVector that) {
+        if (this.length() != that.length())
+            throw new IllegalArgumentException("dimensions disagree");
+        MathVector c = new MathVector(n);
+        for (int i = 0; i < n; i++)
+            c.data[i] = this.data[i] + that.data[i];
+        return c;
+    }
+
+    // return this - that
+    public MathVector minus(MathVector that) {
+        if (this.length() != that.length())
+            throw new IllegalArgumentException("dimensions disagree");
+        MathVector c = new MathVector(n);
+        for (int i = 0; i < n; i++)
+            c.data[i] = this.data[i] - that.data[i];
+        return c;
+    }
+
+    // return the corresponding coordinate
+    public double cartesian(int i) {
+        return data[i];
+    }
+
+    // create and return a new object whose value is (this * factor)
+    @Deprecated
+    public MathVector times(double factor) {
+        MathVector c = new MathVector(n);
+        for (int i = 0; i < n; i++)
+            c.data[i] = factor * data[i];
+        return c;
+    }
+
+    // create and return a new object whose value is (this * factor)
+    public MathVector scale(double factor) {
+        MathVector c = new MathVector(n);
+        for (int i = 0; i < n; i++)
+            c.data[i] = factor * data[i];
+        return c;
+    }
+
+
+    // return the corresponding unit vector
+    public MathVector direction() {
+        if (this.magnitude() == 0.0)
+            throw new ArithmeticException("zero-vector has no direction");
+        return this.times(1.0 / this.magnitude());
+    }
+
+    // return a string representation of the vector
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append('(');
+        for (int i = 0; i < n; i++) {
+            s.append(data[i]);
+            if (i < n-1) s.append(", ");
+        }
+        s.append(')');
+        return s.toString();
+    }
+}
